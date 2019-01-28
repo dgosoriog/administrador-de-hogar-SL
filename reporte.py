9869b1dd8dbf42fc85e39c35f5a09117cba11ce6
Dani9730 <46550631+Dani9730@users.noreply.github.com>
2019-01-27 21:12:56 -0500

    Update reporte.py

--- reporte.py
+++ reporte.py
@@ -1,2 +1,8 @@
 #Archivo en el que se imprimira el reporte de gastos mensuales de la familia
-# david te saluda
+# david
+#reporte pdf
+from reportlab.pdfgen import canvas
+ 
+c = canvas.Canvas("hola_mundo.pdf")
+c.drawString(100,750,"Hola mundo pdf!")
+c.save()
