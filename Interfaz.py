0b3bfc8bf0248fb57123edaf4f2da451cf0c1f7e
wendyG11 <46569936+wendyG11@users.noreply.github.com>
2019-01-31 23:51:17 -0500

    Update Interfaz.py

--- Interfaz.py
+++ Interfaz.py
@@ -2,10 +2,10 @@ from tkinter import *
 ventana = Tk()
 ventana.geometry("450x500")
 ventana.config(bd=30)
-colorFondo  = "Black"
+colorFondo  = "White"
 #DimGray
 ventana.configure(background=colorFondo)
-ventana.config(relief="sunken")
+ventana.config(relief="groove")
 ventana.config(cursor="hand2")
 ventana.title("INGRESOS GASTOS MOVIMIENTOS")
 ventana.resizable(False,False)
@@ -14,12 +14,12 @@ colorLetra = "White"
 ingresos = PhotoImage(file="ingresos.png")
 gastos = PhotoImage(file="gastos.png")
 saldo = PhotoImage(file="saldo.png")
-reporte = PhotoImage(file="reporte.png")
 salir = PhotoImage(file="salir.png")
+reporte = PhotoImage(file="reporte.png")
 bntIng = Button(ventana, image=ingresos,bd=10,relief="groove").grid(row=0,column=0,padx=10,pady=10)
 #place(x=130,y=15)
 btnGastos = Button(ventana, image=gastos,bd=10,relief="groove").grid(row=0,column=1,padx=10,pady=10)
 #place(x=130,y=160)
 btnSaldo = Button(ventana,image=saldo,bd=10,relief="groove").grid(row=1,column=0,padx=10,pady=10)
-btnReporte = Button(ventana,image=reporte,bd=10,relief="groove").grid(row=1,column=1,padx=10,pady=10)
+btnSaldo = Button(ventana,image=reporte,bd=10,relief="groove").grid(row=1,column=1,padx=10,pady=10)
 ventana.mainloop()
