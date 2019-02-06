933243feed8162e89a0c3e9308f0a26170beca44
wendyG11 <46569936+wendyG11@users.noreply.github.com>
2019-02-06 11:18:02 -0500

    terminado

--- PantallaGastos.py
+++ PantallaGastos.py
@@ -1,20 +1,8 @@
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk, messagebox
 import pymysql
 from tkinter import messagebox
-
-import os
-import shutil
-from tkinter import messagebox
 from tkinter import ttk
-from tkinter.filedialog import askopenfilename
-from tkinter import filedialog
-from distutils import command
-import shutil
-from tkinter import messagebox
-from tkinter import ttk
-from tkinter.filedialog import askopenfilename
 from tkinter import Label,Tk
 
 from tkinter import filedialog
@@ -66,6 +54,21 @@ class PantallaGastos(tk.Frame):
         except:
             print("No se ha podido ingresar")
 
+        cursor.execute("SELECT VERSION()")
+        sql3 = "select sum(monto) from ingresos"
+
+        try:
+            cursor.execute(sql3)
+            results = cursor.fetchall()
+            for row in results:
+                t = int(row[0])
+                print(t)
+        except:
+            print("Error")
+
+        gastos.totalGastos.set(t)
+        return gastos.totalGastos
+
     def __init__(gastos, parent, controller):
      tk.Frame.__init__(gastos, parent)
      gastos.controller = controller
@@ -81,41 +84,39 @@ class PantallaGastos(tk.Frame):
      gastos.montoG=StringVar()
      gastos.descripcionG=StringVar()
      gastos.fechaG=StringVar()
+     gastos.totalGastos=StringVar()
 
 
-     labelMonto=Label(gastos,text="INGRESA EL MONTO DEL GASTO:",bg=colorFondo,fg = colorLetra,font=("Comic Sans MS",8)).grid(row=0,column=0,padx=10,pady=10)
+     labelMonto=Label(gastos,text="INGRESA EL MONTO DEL GASTO:",bg=colorFondo,fg = colorLetra,font=controller.title_font).grid(row=0,column=0,padx=10,pady=10)
      global txtMonto
-     txtMonto=Entry(gastos,textvariable=gastos.montoG,bd=5,relief="sunken",width=22,font=("Comic Sans MS",8))
+     txtMonto=Entry(gastos,textvariable=gastos.montoG,bd=5,relief="sunken",width=25,font=("Helvetica",9))
      txtMonto.grid(row=0,column=1,padx=10,pady=10)
 
-     labelIngreso = Label(gastos,text="CATEGORÍA DEL GASTO: ",bg=colorFondo,fg = colorLetra,font=("Comic Sans MS",8)).grid(row=1,column=0,padx=10,pady=10)
+     labelIngreso = Label(gastos,text="CATEGORÍA DEL GASTO: ",bg=colorFondo,fg = colorLetra,font=controller.title_font).grid(row=1,column=0,padx=10,pady=10)
 
      combo = ttk.Combobox(gastos,textvariable=gastos.catG)
      combo.grid(row=1,column=1,padx=10,pady=10)
      combo['values'] = (nombre)
-     combo.config(font=("Comic Sans MS",8))
+     combo.config(font=("Helvetica",9))
 
-     labelFecha = Label(gastos, text="FECHA:", bg=colorFondo, fg=colorLetra, font=("Rouge", 10)).grid(row=3, column=0,padx=10, pady=10)
+     labelFecha = Label(gastos, text="FECHA:", bg=colorFondo, fg=colorLetra, font=controller.title_font).grid(row=2, column=0,padx=10, pady=10)
      global txtFecha
-     txtFecha = Entry(gastos, textvariable=gastos.fechaG, text="año/mes/dia", bd=5, relief="ridge")
-     txtFecha.grid(row=3,column=1,padx=10,pady=10)
+     txtFecha = Entry(gastos, textvariable=gastos.fechaG, text="año/mes/dia", bd=5, relief="sunken",width=25,font=("Helvetica",9))
+     txtFecha.grid(row=2,column=1,padx=10,pady=10)
 
-     labelDescripcion=Label(gastos,text="DESCRIPCIÓN:",bg=colorFondo,fg = colorLetra,font=("Comic Sans MS",8)).grid(row=2,column=0,padx=10,pady=10)
+     labelDescripcion=Label(gastos,text="DESCRIPCIÓN:",bg=colorFondo,fg = colorLetra,font=controller.title_font).grid(row=3,column=0,padx=10,pady=10)
      global txtDescripcion
-     txtDescripcion=Entry(gastos,bd=5,relief="sunken",textvariable=gastos.descripcionG)
-     txtDescripcion.grid(row=2,column=1,padx=10,pady=10)
+     txtDescripcion=Entry(gastos,bd=5,relief="sunken",textvariable=gastos.descripcionG,width=25,font=("Helvetica",9))
+     txtDescripcion.grid(row=3,column=1,padx=10,pady=10)
 
 
 
-     labelSaldo=Label(gastos,text="TOTAL DE GASTOS:",bg=colorFondo,fg = colorLetra,font=("Comic Sans MS",8)).grid(row=4,column=0,padx=10,pady=10)
-     txtSaldo=Entry(gastos,bd=5,relief="sunken",width=25,bg="#FFFF00",fg="Red").grid(row=4,column=1,padx=10,pady=10)
+     global txtSaldo
+     labelSaldo=Label(gastos,text="TOTAL DE GASTOS:",bg=colorFondo,fg = "Red",font=controller.title_font).grid(row=4,column=0,padx=10,pady=10)
+     txtSaldo=Entry(gastos,text=gastos.totalGastos,bd=5,relief="sunken",width=25,bg="#FFFF00",fg="Red",font=("Helvetica",9)).grid(row=4,column=1,padx=10,pady=10)
 
-     btnAtras = Button(gastos,text="ATRAS",bd=10,relief="groove",command=lambda: controller.show_frame("PantallaMenu")).grid(row=5,column=0,padx=10,pady=10)
-     btnGuardar = Button(gastos,text="GUARDAR",bd=10,relief="groove",command = gastos.validacion).grid(row=5,column=1,padx=10,pady=10)
-
-
-
+     btnAtras = Button(gastos,text="ATRAS",font=controller.title_font,bd=10,relief="groove",command=lambda: controller.show_frame("PantallaMenu")).grid(row=5,column=0,padx=10,pady=10)
+     btnGuardar = Button(gastos,text="GUARDAR",font=controller.title_font,bd=10,relief="groove",command = gastos.validacion).grid(row=5,column=1,padx=10,pady=10)
 
 
-
-
+         
