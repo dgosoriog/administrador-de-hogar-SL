b7a68ddddec9c82b7fb84fdf0b585fe00b92147f
wendyG11 <46569936+wendyG11@users.noreply.github.com>
2019-02-06 11:10:01 -0500

    Update PantallaMenu.py

--- PantallaMenu.py
+++ PantallaMenu.py
@@ -2,11 +2,6 @@ import tkinter as tk
 from tkinter import *
 class PantallaMenu(tk.Frame):
 
-#gastos = PhotoImage(file="gastos.png")
-#saldo = PhotoImage(file="saldo.png")
-#salir = PhotoImage(file="salir.png")
-
-
     def __init__(self, parent, controller):
 
      tk.Frame.__init__(self, parent)
@@ -26,7 +21,7 @@ class PantallaMenu(tk.Frame):
      reporte = tk.PhotoImage(file="reporte.png")
      reporte.imge=reporte
      labelActividad=Label(self,text="SELECCIONA TU ACTIVIDAD:",bg="White",fg = "Orange",font=("Helvetica",12)).grid(row=0,column=0,padx=10,pady=10)
-     btnIng =    Button(self,text="INGRESOS",image=ingresos,compound="top",bd=10,relief="groove",command=lambda: controller.show_frame("PantallaIngresos")).grid(row=1,column=0,padx=10,pady=10)
-     btnGastos = Button(self,text="GASTOS", image=gastos,compound="top",bd=10,relief="groove",command=lambda: controller.show_frame("PantallaGastos")).grid(row=1,column=1,padx=10,pady=10)
-     btnSaldo = Button(self,text="SALDO",image=saldo,compound="top",bd=10,relief="groove",command=lambda: controller.show_frame("PantallaSaldo")).grid(row=2,column=0,padx=10,pady=10)
-     btnReporte = Button(self,text="REPORTES",image=reporte,compound="top",bd=10,relief="groove").grid(row=2,column=1,padx=10,pady=10)
+     btnIng =    Button(self,text="INGRESOS",font=controller.title_font,image=ingresos,compound="top",bd=10,relief="groove",command=lambda: controller.show_frame("PantallaIngresos")).grid(row=1,column=0,padx=10,pady=10)
+     btnGastos = Button(self,text="GASTOS",font=controller.title_font, image=gastos,compound="top",bd=10,relief="groove",command=lambda: controller.show_frame("PantallaGastos")).grid(row=1,column=1,padx=10,pady=10)
+     btnSaldo = Button(self,text="SALDO",font=controller.title_font,image=saldo,compound="top",bd=10,relief="groove",command=lambda: controller.show_frame("PantallaSaldo")).grid(row=2,column=0,padx=10,pady=10)
+     btnReporte = Button(self,text="REPORTES",font=controller.title_font,image=reporte,compound="top",bd=10,relief="groove",command=lambda: controller.show_frame("PantallaReportes")).grid(row=2,column=1,padx=10,pady=10)
